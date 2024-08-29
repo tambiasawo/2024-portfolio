8b6b44ffba4c1789089995f28dbade183c34ebfc
Tambi <tambi.asawo@verticalmotion.ca>
2024-08-29 11:06:13 -0700

    cleanup

--- app/components/ui/TextGenerateEffect.tsx
+++ app/components/ui/TextGenerateEffect.tsx
@@ -13,7 +13,6 @@ export const TextGenerateEffect = ({
   const [scope, animate] = useAnimate();
   let wordsArray = words.split(" ");
   useEffect(() => {
-    console.log(wordsArray);
     animate(
       "span",
       {
@@ -51,7 +50,7 @@ export const TextGenerateEffect = ({
       {/* mt-4 to my-4 */}
       <div className="my-4">
         {/* remove  text-2xl from the original */}
-        <div className=" dark:text-white text-black leading-snug tracking-wide">
+        <div className=" dark:text-white text-black leading-snug tracking-tighter">
           {renderWords()}
         </div>
       </div>
