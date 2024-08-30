047150ac8d567c535272175174f2cb4c96b67339
Tambi <tambi.asawo@verticalmotion.ca>
2024-08-30 02:07:25 -0700

    fix:change title of id=6 object

--- data/index.ts
+++ data/index.ts
@@ -49,7 +49,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a Next.js E-commerce Store",
+    title: "Currently building a Web3 Digital Identity App",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
