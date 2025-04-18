f2bbb8b1ec447106bbde15b8c34e476b0da1b6ab
Tambi <tambi@takingroot.org>
2025-04-17 18:30:26 -0700

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -2,7 +2,7 @@ export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
   { name: "Testimonials", link: "#testimonials" },
-  { name: "Contact", link: "#contact" },
+  { name: "Contact", link: "#contact" }, 
 ];
 
 export const gridItems = [
