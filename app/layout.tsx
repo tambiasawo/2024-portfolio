393d816cdef38f168ef600830997a4dea734a1e8
Tambi <tambi.asawo@verticalmotion.ca>
2025-06-24 21:33:15 -0700

    feat: add favico

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Tambi's Portfolio",
   description: "My Experience in a Scroll",
+  icons: {
+    icon: "/tambi.png",
+  },
 };
 
 export default function RootLayout({
