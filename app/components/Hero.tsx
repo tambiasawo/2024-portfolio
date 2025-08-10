363324ab256598d86776b92bb232a0c073be6a11
Tambi <tambi.asawo@verticalmotion.ca>
2025-08-10 16:07:34 -0700

    refactor

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -39,7 +39,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 md:text-lg lg:text-2xl">
-            Hi! I&apos;m Tambi, a Senior Fullstack Engineer based in Vancouver,
+            Hi! I&apos;m Tambi, a Senior Fullstack (frontend) Engineer based in Vancouver,
             BC.
           </p>
 
