7840c39f251f3d69bc462fc56a36dbd1f2bfffc1
Tambi <tambi.asawo@yahoo.com>
2026-08-08 02:46:18 +0800

    Update title for AWS certification entry

--- data/index.ts
+++ data/index.ts
@@ -49,7 +49,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently working on my AWS Solutions Architect Certification",
+    title: "AWS Solutions Architect Certified",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
